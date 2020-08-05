e89f3e16c1614173415876ade6cf4797103467a3
Josh Raiborde <joshsrai@yahoo.com>
2020-08-05 14:07:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,15 +8,25 @@ class Dog
     @name = name
     self.save
   end
+<<<<<<< HEAD
   
   def name
     @name
   end
+=======
+>>>>>>> 2b0e93ded848eac9f09e4c80804271690e47c922
 
   def self.all
     @@all
   end
 
+<<<<<<< HEAD
+=======
+  def self.clear_all
+    @@all = [] 
+  end
+
+>>>>>>> 2b0e93ded848eac9f09e4c80804271690e47c922
   def self.print_all
     @@all.each do |a| puts a.name
     end
@@ -25,9 +35,15 @@ class Dog
   def save
     @@all << self
   end
+<<<<<<< HEAD
   
   def self.clear_all
     @@all = [] 
+=======
+
+  def name
+    @name
+>>>>>>> 2b0e93ded848eac9f09e4c80804271690e47c922
   end
 
 end
